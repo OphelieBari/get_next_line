f2b492a1d8d9287438158a89de521fabb90ba5b3
Ophelie BARIBAUD <obaribau@e1r5p18.42.fr>
2020-02-11 16:52:09 +0100

    plus qu'a normer

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: opheliebaribaud <marvin@42.fr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/06 21:03:02 by ophelieba         #+#    #+#             */
-/*   Updated: 2020/02/09 15:10:27 by ophelieba        ###   ########.fr       */
+/*   Updated: 2020/02/11 16:50:54 by obaribau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,65 +18,84 @@ int	get_next_line(int fd, char **line)
 	char		buf[BUFFER_SIZE + 1];
 	int		i;
 	static int	ret_read;
+	char		*tmp;
 
 	i = 0;
+	if (!BUFFER_SIZE || fd < 0 || !line)
+		return (-1);
 	if (save == NULL)
 	{
 		ret_read = read(fd, buf, BUFFER_SIZE);
 		if (ret_read < 0)
-			return (-1);
+			return (-1);		
+		buf[ret_read] = '\0';
 		save = ft_strdup_mod(buf, ft_strlen(buf));
 	}
 	if(ft_strchr(save, '\n'))
 	{
-		if (save[0] == '\n')
-		{
-			*line = NULL;
-			save = ft_strdup_mod(&save[1], ft_strlen(&save[1]));
-			return (1);
-		}
-		while (save[i] != '\n')
+		while (save[i] && save[i] != '\n')
 			i++;
 		*line = ft_strdup_mod(save, i);
-		save = ft_strdup_mod(&save[i + 1], ft_strlen(&save[i + 1]));
+		tmp = ft_strdup_mod(&save[i + 1], ft_strlen(&save[i + 1]));
+		free(save);
+		save = NULL;
+		save = ft_strdup_mod(tmp, ft_strlen(tmp));
+		free(tmp);
 		return (1);
 	}
-	if (ret_read > 0)
+	if (ret_read >= 0)
 	{
 		while (!(ft_strchr(save, '\n')) && ret_read > 0)
 		{
 			ret_read = read(fd, buf, BUFFER_SIZE);
-			if (ret_read > 0)
-				save = ft_strjoin(save, buf);
 			if (ret_read < 0)
 				return (-1);
+			buf[ret_read] = '\0';
+			if (ret_read > 0)
+			{
+				tmp = ft_strjoin(save, buf);
+				free(save);
+				save = NULL;
+				save = ft_strdup_mod(tmp, ft_strlen(tmp));
+				free(tmp);
+			}	
 		}
-		while (save[i] != '\n')
-			i++;
-		*line = ft_strdup_mod(save, i);
-		save = ft_strdup_mod(&save[i + 1], ft_strlen(&save[i + 1]));
+		if (ret_read > 0)
+		{
+			while (save[i] && save[i] != '\n')
+				i++;
+			*line = ft_strdup_mod(save, i);
+			tmp = ft_strdup_mod(&save[i + 1], ft_strlen(&save[i+ 1]));
+			free(save);
+			save = NULL;
+			save = ft_strdup_mod(tmp, ft_strlen(tmp));
+			free(tmp);
+		}
 		if (ret_read == 0)
 		{
-			save = NULL;
+			*line = ft_strdup_mod(save, ft_strlen(save));
 			free(save);
+			save = NULL;
 			return (0);
 		}
-		return (1);
+		return(1);
 	}
 	return (0);
 }
 
 #include <stdio.h>
 
-int main()
+/*int main()
 {
 	int             fd;
 	int             i;
 	int             j;
 	char    		*line = 0;
 	char			*lineadress[66];
+	int	w;
 
 	j = 1;
+	w = 1;
 	printf("\n==========================================\n");
 	printf("========== TEST 1 : The Alphabet =========\n");
 	printf("==========================================\n\n");
@@ -96,4 +115,4 @@ int main()
 	free(line);
 	close(fd);
 	return (0);
-}
+}*/
